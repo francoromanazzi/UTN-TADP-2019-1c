28feff9d979ef07c9e0a3828d9b4c3ff19cf70cd
Franco21198 <francoromanazzi@hotmail.com>
2019-06-09 18:24:29 -0300

    Agrego test, que por ahora fallan

--- scala/src/test/scala/MusicParser.scala
+++ scala/src/test/scala/MusicParser.scala
@@ -58,5 +58,48 @@ class MusicParserTest extends FreeSpec with Matchers {
         }
       }
     }
+
+    "when fed a text that is a single note repeated multiple times times" - {
+      "it parses a list with that note appearing two times" in {
+        assertParsesSucceededWithResult(new MusicParser("2x(A)").parse(), List(A, A))
+      }
+
+      "it parses a list with that note appearing three times" in {
+        assertParsesSucceededWithResult(new MusicParser("3x(A)").parse(), List(A, A, A))
+      }
+    }
+
+    "when fed a text that contains a wrong note inside a repetition" - {
+      "it fails" in {
+        assertParseFailed(new MusicParser("3x(A J)").parse(), List(A))
+      }
+    }
+
+    "when fed a text that is multiple notes repeated multiple times" - {
+      "it parses a list with those two notes appearing two times" in {
+        assertParsesSucceededWithResult(new MusicParser("2x(A B)").parse(), List(A, B, A, B))
+      }
+
+      "it parses a list with those two notes appearing three times" in {
+        assertParsesSucceededWithResult(new MusicParser("3x(A B)").parse(), List(A, B, A, B, A, B))
+      }
+    }
+
+    "when fed a text contains multiple notes repeated multiple times along with regular notes" - {
+      "it parses a list with those two notes appearing two times after a regular note" in {
+        assertParsesSucceededWithResult(new MusicParser("C 2x(A B)").parse(), List(C, A, B, A, B))
+      }
+
+      "it parses a list with those two notes appearing two times before a regular note" in {
+        assertParsesSucceededWithResult(new MusicParser("2x(A B) C").parse(), List(A, B, A, B, C))
+      }
+    }
+
+    "when fed a text that contains repetitions inside another repetition" - {
+      "it parses a list with all nested repetitions resolved into regular notes" in {
+        assertParsesSucceededWithResult(new MusicParser("2x(A B 3x(F G 2x(A))) F B E").parse(),
+          List(A, B, F, G, A, A, F, G, A, A, F, G, A, A, A, B, F, G, A, A, F, G, A, A, F, G, A, A, F, B, E))
+      }
+    }
   }
 }
